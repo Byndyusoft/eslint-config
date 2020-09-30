a8eec84eab7be0b3e19562974054ab7d016a8ef2
Eugene Abrosimov <hi@sadcitizen.me>
2020-09-30 15:43:21 +0500

    add flow config

--- flow.js
+++ flow.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+    extends: ['plugin:flowtype/recommended'],
+    plugins: ['flowtype'],
+    rules: {},
+    settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true
+        }
+    }
+};
