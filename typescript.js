b831a9e0eda63ef97b6ccb1a42703088d0af4a7a
Eugene Abrosimov <hi@sadcitizen.me>
2020-09-30 16:20:46 +0500

    add typescript config

--- typescript.js
+++ typescript.js
@@ -1,1 +1,20 @@
-module.exports = {};
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
+        './configs/best-practices.js',
+        './configs/ecmascript.js',
+        './configs/errors.js',
+        './configs/variables.js',
+        './configs/import.js'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {}
+};
