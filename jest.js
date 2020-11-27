0f7fef2b51929af5410ce692ba86652ad3aafc72
Eugene Abrosimov <sadcitizen@yandex.ru>
2020-11-27 10:27:57 +0500

    update eslint rules for jest

--- jest.js
+++ jest.js
@@ -1,13 +1,41 @@
 module.exports = {
     env: {
-        jest: true
+        'jest/globals': true
     },
     extends: ['plugin:jest/recommended'],
     plugins: ['jest'],
-    rules: {},
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
     settings: {
         jest: {
             version: 26
         }
-    }
+    },
+    overrides: [
+        {
+            files: ['*.{spec, test, tests}.*', '**/__tests__/**'],
+            rules: {
+                'jest/consistent-test-it': [
+                    'error',
+                    {
+                        fn: 'test',
+                        withinDescribe: 'test'
+                    }
+                ],
+                'jest/no-duplicate-hooks': 'error',
+                'jest/no-test-return-statement': 'error',
+                'jest/prefer-called-with': 'warn',
+                'jest/prefer-hooks-on-top': 'warn',
+                'jest/prefer-strict-equal': 'warn',
+                'jest/prefer-to-be-null': 'warn',
+                'jest/prefer-to-contain': 'warn',
+                'jest/prefer-to-have-length': 'warn',
+                'jest/prefer-todo': 'warn',
+                'jest/require-to-throw-message': 'warn',
+                'jest/require-top-level-describe': 'warn'
+            }
+        }
+    ]
 };
