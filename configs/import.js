039c590ea016d2700d47531a50f91bbb8f0392fe
Eugene Abrosimov <hi@sadcitizen.me>
2020-11-29 16:09:27 +0500

    update rules for import plugin

--- configs/import.js
+++ configs/import.js
@@ -1,5 +1,12 @@
 module.exports = {
     extends: ['plugin:import/errors', 'plugin:import/warnings'],
     plugins: ['import'],
-    rules: {}
+    rules: {
+        'import/first': 'warn',
+        'import/no-cycle': 'error',
+        'import/no-mutable-exports': 'error',
+        'import/no-self-import': 'error',
+        'import/no-unused-modules': 'warn',
+        'import/no-useless-path-segments': 'warn'
+    }
 };
