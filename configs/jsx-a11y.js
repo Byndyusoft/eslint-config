ace5381c4e43760dd43a33f5f5e8f2504d21462a
Eugene Abrosimov <hi@sadcitizen.me>
2021-02-03 10:37:15 +0500

    update rules

--- configs/jsx-a11y.js
+++ configs/jsx-a11y.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ['plugin:jsx-a11y/recommended'],
     plugins: ['jsx-a11y'],
     rules: {
-        'jsx-a11y/click-events-have-key-events': 'off'
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/accessible-emoji': 'warn'
     }
 };
